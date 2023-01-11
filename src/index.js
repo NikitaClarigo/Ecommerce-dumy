28f9a6f063c673e29c436e5e185215030c2845b7
Nikita_clarigo <nikita.clarigoinfotech@gmail.com>
2023-01-11 12:53:06 +0530

    changes 15

--- src/index.js
+++ src/index.js
@@ -1,40 +1,18 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
-// import App from './App';
+import App from './App';
 import reportWebVitals from './reportWebVitals';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import 'bootstrap/dist/js/bootstrap.bundle.min';
-import { BrowserRouter,Route, Routes } from 'react-router-dom';
+import { BrowserRouter} from 'react-router-dom';
 import './Screens/Home/home.css';
-import Main from './Screens/Main/Main';
-import Men from './Screens/Men/Men';
-import Women from './Screens/Women/Women';
-import Kids from './Screens/Kids/Kids';
-import HomeandLiving from './Screens/HomeandLiving/HomeandLiving';
-import Beauty from './Screens/Beauty/Beauty';
-import Login from './Screens/Login/Login';
-import Dashboard from './Screens/Dashboard/Dashboard';
-import Wishlist from './Screens/Wishlist/Wishlist';
-import Cart from './Screens/Cart/Cart';
 // import Header from './Components/Header/Header';
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
     <BrowserRouter>
-    <Routes>
-            <Route path='/Login' element={<Login />} />
-            <Route exact path='/' element={<Dashboard />} >
-              <Route path='Dashboard' element={<Main />} />
-              <Route path='Men' element={<Men />} />
-              <Route path='Women' element={<Women />} />
-              <Route path='Kids' element={<Kids />} />
-              <Route path='HomeandLiving' element={<HomeandLiving />} />
-              <Route path='Beauty' element={<Beauty />} />
-              <Route path='Wishlist' element={<Wishlist />} />
-              <Route path='Cart' element={<Cart />} />
-            </Route>
-          </Routes>
+    <App />
     </BrowserRouter>
   </React.StrictMode>
 );
