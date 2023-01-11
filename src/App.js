9e01c964c7afad1bfabf107a0c41925098fcfa7c
Nikita_clarigo <nikita.clarigoinfotech@gmail.com>
2023-01-11 10:45:22 +0530

    changes

--- src/App.js
+++ src/App.js
@@ -1,10 +1,11 @@
 import React from 'react';
-import Home from './Screens/Home/Home';
+// import Home from './Screens/Home/Home';
 
 export default function App() {
   return (
     <div>
-      <Home />
+      {/* <Home /> */}
+      <h1>dsbfcdsbnxdk</h1>
     </div>
   )
 }
